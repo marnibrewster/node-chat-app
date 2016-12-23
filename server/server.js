df338e39bc5916994d1bce74abd48ab23afa22f7
Marni <marniduffy@gmail.com>
2016-12-23 09:25:29 -0500

    added broadcast of newMessage

--- server/server.js
+++ server/server.js
@@ -13,10 +13,20 @@ app.use(express.static(publicPath))
 
 io.on('connection', (socket) =>{
   console.log('new user connected');
+    // socket.emit('newMessage', {
+    //   from: "mjb",
+    //   text: "message from server",
+    //   createdAt: 233232
+    // });
     socket.emit('newMessage', {
-      from: "mjb",
-      text: "message from server",
-      createdAt: 233232
+      from: 'Admin',
+      text: 'Welcome to the chat app',
+      createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'New user joined',
+      createdAt: new Date().getTime()
     });
     socket.on('createMessage', (message) => {
       console.log('create message', message);
@@ -25,6 +35,11 @@ io.on('connection', (socket) =>{
         text: message.text,
         createdAt: new Date().getTime()
       });
+      // socket.broadcast.emit('newMessage', {
+      //     from: message.from,
+      //     text: message.text,
+      //     createdAt: new Date().getTime()
+      // });
     });
   // socket.on('disconnect', (socket) => {
   //   console.log('user disconnected from server')
