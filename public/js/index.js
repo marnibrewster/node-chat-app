c0b10b849a2ffc0a2669628001da02d6795cb4da
Marni <marniduffy@gmail.com>
2016-12-26 21:02:30 -0500

    added mustache templates

--- public/js/index.js
+++ public/js/index.js
@@ -7,21 +7,26 @@ var messageTextBox = jQuery('[name=message]');
 
 socket.on('newMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  jQuery('#messages').append(li);
-  messageTextBox.val("");
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My Current Location</a>');
-
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url); //sets url of href to message.url
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    createdAt: formattedTime,
+    url: message.url
+  });
+  
+  jQuery('#messages').append(html);
 });
 
 socket.on('disconnect', function () {
